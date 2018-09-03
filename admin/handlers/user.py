96dc7eacd0abadc9d42556749c52d8b0084a989e
陈帅 <shuai.chen@moji.com>
2018-09-03 10:19:26 +0800

    update

--- admin/handlers/user.py
+++ admin/handlers/user.py
@@ -17,9 +17,10 @@ from towgo.utils.extend import Odict
 
 from admin.common.result import returns
 from app.models.goods import GoodsInfo
-from app.models.user import UserInfo, UserDeliverAddress, UserGoods
+from app.models.user import UserInfo, UserDeliverAddress, UserGoods, WechatUser
 from app.services.goods import GoodsService
-from app.services.user import UserInfoService, UserDeliverAddressService, UserGoodsService
+from app.services.user import UserInfoService, UserDeliverAddressService, UserGoodsService, \
+    WechatUserService
 
 
 class LookUsertHandler(TornadoHttpHandler):
@@ -77,10 +78,64 @@ class LookUsertHandler(TornadoHttpHandler):
             uinfo['goods'] = ugl
             uinfo['address'] = udal    
     
-            return self.render_html('admin/look_user.html',uid=user_id,msg=None,**uinfo) 
+            return self.render_html('admin/look_user.html',uid=user_id,account=account,msg=None,**uinfo) 
             
         return self.render_html('admin/look_user.html', uid=user_id,account=account,msg=u'该用户不存在!') 
+
+class WechatUserHandler(TornadoHttpHandler):
+
+    @returns(need_login=True)  
+    def _get(self):
+        return self.render_html('admin/wechat_user.html', uid='',msg=u'请输入用户ID!') 
+            
+    @returns(need_login=True)  
+    def _post(self):
+        user_id =  self.get_argument("uid", '')
+        user_info = None
+        if len(user_id) > 0:
+            user_info = WechatUserService(WechatUser).get(id=int(user_id))
+        else:
+            return self.render_html('admin/wechat_user.html', uid='',msg=u'请输入用户ID!')      
+            
+        if user_info:
+            uinfo = {}
+            uinfo['user_id'] = user_info.id
+            uinfo['nickname'] = user_info.nickname
+            uinfo['deliver_address_id'] = user_info.deliver_address_id
+
+            user_id = user_info.id
+            user_adress = UserDeliverAddressService(UserDeliverAddress).gets(user_id=user_id, status=1)
+            user_goods = UserGoodsService(UserGoods).gets(user_id=user_id)
+                        
+            ugl = []
+            if user_goods:
+                for item in user_goods:
+                    d = {}
+                    d['goods_id'] = item.goods_id
+                    d['goods_name'] = item.goods_name
+                    d['number'] = item.number
+                    d['deliver_date'] = item.deliver_date
+                    d['status'] = {1:u'待发货',2:u'已发货',3:u'已完成'}.get(item.status)
+                    ugl.append(Odict(d))
+                
+            udal = []  
+            if user_adress:
+                for item in  user_adress:
+                    d = {} 
+                    d['name'] = item.name
+                    d['phone'] = item.phone
+                    d['address'] = u'{0}{1}'.format(item.region_name, item.address)
+                    d['region_name'] = item.region_name
+                    d['region_id'] = item.region_id
+                    udal.append(Odict(d))
+                
+            uinfo['goods'] = ugl
+            uinfo['address'] = udal    
     
+            return self.render_html('admin/wechat_user.html',uid=user_id,msg=None,**uinfo) 
+            
+        return self.render_html('admin/wechat_user.html', uid=user_id,msg=u'该用户不存在!')         
+            
 class AdPicUploadHandler(TornadoHttpHandler):  
 
     def get_pics(self):
